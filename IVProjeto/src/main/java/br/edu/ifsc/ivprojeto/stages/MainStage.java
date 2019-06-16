aebc9076eac7e95eaceabbac6a9179e4667aff24
fernandonowak <49540289+fernandonowak@users.noreply.github.com>
2019-06-15 22:37:47 -0300

    Inserindo imagem
    plano de fundo

--- IVProjeto/src/main/java/br/edu/ifsc/ivprojeto/stages/MainStage.java
+++ IVProjeto/src/main/java/br/edu/ifsc/ivprojeto/stages/MainStage.java
@@ -1,18 +1,23 @@
 package br.edu.ifsc.ivprojeto.stages;
 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MainStage {
 
 	public MainStage(Stage stage) throws Exception {
-		
-			// creating the classes hierarchy (pane -> scene -> stage)
-			AnchorPane pane = new AnchorPane();
-			pane.setPrefSize(170, 190);
-			Scene scene = new Scene(pane);
-			stage.setScene(scene);
+
+		// creating the classes hierarchy (pane -> scene -> stage)
+		AnchorPane pane = new AnchorPane();
+		pane.setPrefSize(1000, 750);
+		Scene scene = new Scene(pane);
+		stage.setScene(scene);
+
+		stage.show();
 	}
 
 }
