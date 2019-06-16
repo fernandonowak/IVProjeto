0624eabd8872657c01670b8f2b79c6c23fbe32cb
fernandonowak <49540289+fernandonowak@users.noreply.github.com>
2019-06-16 16:23:04 -0300

    plano de fundo
    inserindo plano de fundo na MainStage

--- IVProjeto/src/main/java/br/edu/ifsc/ivprojeto/stages/MainStage.java
+++ IVProjeto/src/main/java/br/edu/ifsc/ivprojeto/stages/MainStage.java
@@ -1,9 +1,16 @@
 package br.edu.ifsc.ivprojeto.stages;
 
+import java.io.FileInputStream;
+
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -13,8 +20,17 @@ public class MainStage {
 
 		// creating the classes hierarchy (pane -> scene -> stage)
 		AnchorPane pane = new AnchorPane();
-		pane.setPrefSize(1000, 750);
+		pane.setPrefSize(900, 650);
 		Scene scene = new Scene(pane);
+		FileInputStream input = new FileInputStream("C:\\Users\\Fernando\\Documents\\GitHub\\IVProjeto\\IVProjeto\\src\\main\\java\\br\\edu\\ifsc\\ivprojeto\\stages\\imagem1.jpg");
+		Image image = new Image(input); 
+		BackgroundImage backgroundimage = new BackgroundImage(image,  
+                BackgroundRepeat.NO_REPEAT,  
+                BackgroundRepeat.NO_REPEAT,  
+                BackgroundPosition.DEFAULT,  
+                   BackgroundSize.DEFAULT);
+		 Background background = new Background(backgroundimage);
+		 pane.setBackground(background);
 		stage.setScene(scene);
 
 		stage.show();
